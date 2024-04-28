1757070cb6bc9e6873974eba19eb7ca6c063ed4c
kts of kettek <kts@kettek.net>
2024-04-27 18:34:03 -0700

    Implement iframe message passing and handling
    This allows full styling with an embedded iframe.

--- game/main.go
+++ game/main.go
@@ -7,6 +7,7 @@ import (
 	"syscall/js"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -49,6 +50,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for _, r := range g.rects {
 		vector.DrawFilledRect(screen, float32(r.x), float32(r.y), float32(r.width), float32(r.height), r.color, true)
 	}
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("%dx%d", g.w, g.h))
 }
 
 func (g *Game) Layout(ow, oh int) (int, int) {
@@ -60,23 +62,18 @@ func (g *Game) Layout(ow, oh int) (int, int) {
 func main() {
 	var game Game
 
-	var randCb js.Func
-	randCb = js.FuncOf(func(this js.Value, p []js.Value) any {
-		game.rects = append(game.rects, MakeRandomRect(game.w, game.h))
-		str := ""
-		for i, r := range game.rects {
-			str += fmt.Sprintf("%d: x=%d, y=%d, w=%d, h=%d, c=%d|%d|%d|%d\n", i, r.x, r.y, r.width, r.height, r.color.R, r.color.G, r.color.B, r.color.A)
+	js.Global().Get("window").Call("addEventListener", "message", js.FuncOf(func(this js.Value, p []js.Value) any {
+		event := p[0].Get("data").String()
+		switch event {
+		case "button:random":
+			game.rects = append(game.rects, MakeRandomRect(game.w, game.h))
+		case "button:clear":
+			game.rects = []Rect{}
+		default:
+			fmt.Println("Unknown event:", event)
 		}
-		js.Global().Get("console").Call("log", str)
 		return nil
-	})
-	js.Global().Get("document").Call("getElementById", "randomButton").Call("addEventListener", "click", randCb)
-	var clearCb js.Func
-	clearCb = js.FuncOf(func(this js.Value, p []js.Value) any {
-		game.rects = []Rect{}
-		return nil
-	})
-	js.Global().Get("document").Call("getElementById", "clearButton").Call("addEventListener", "click", clearCb)
+	}))
 
 	ebiten.SetWindowSize(320, 240)
 	ebiten.SetWindowTitle("Hello, World!")
